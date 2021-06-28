42267117881cd901ee9e0b5ad81c4eaccc7c26fe
Valtteri A <vjaarn@utu.fi>
2021-06-28 17:09:20 +0300

    tehtävät 2.1 - 2.3 tehty

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,55 +1,71 @@
 import React from 'react'
 
-const Header = (props) => {
-  return(
-    <h1>{props.course.name}</h1>
-  )
-}
-
-const Content = (props) => {
-  let partselements = props.parts.map(part => <p key={part.name}>{part.name} {part.excercises}</p>)
-  return(
-    <div>
-      {partselements}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  let sum = 0
-  props.parts.forEach(part => {
-    sum = sum + part.exercises
-  })
-
-  return(
-    <p>Number of excercises {sum}</p>
-  )
-}
-
 const App = () => {
   const course = {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
+  return (
+    <div>
+      <Course course={course} />
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  const total = parts.reduce((sum, part) => sum += part.exercises, 0)
+  return(
+    <p>total of {total}</p>
+  )
+}
+
+const Course = ({course}) => {
+
   return(
     <div>
-      <Header course={course} />
+      <Header content={course.name}/>
       <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+const Header = ({content}) => {
+  return(
+    <h1>{content}</h1>
+  )
+}
+
+const Content = ({parts}) => {
+  return(
+    <div>
+      {parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises}/>)}
+    </div>
+  )
+}
+
+const Part = ({name, exercises}) => {
+  return(
+    <div>
+      <p>{name} {exercises}</p>
     </div>
   )
 }
