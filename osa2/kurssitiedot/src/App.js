3d855498452bc428cc37fb83b032b1a9c249c5ff
Valtteri A <vjaarn@utu.fi>
2021-06-28 16:18:16 +0300

    osa 2 kansio luotu ja kurssitiedot-tehätävä kopioitu osasta 1

---
+++ osa2/kurssitiedot/src/App.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const Header = (props) => {
+  return(
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  let partselements = props.parts.map(part => <p key={part.name}>{part.name} {part.excercises}</p>)
+  return(
+    <div>
+      {partselements}
+    </div>
+  )
+}
+
+const Total = (props) => {
+  let sum = 0
+  props.parts.forEach(part => {
+    sum = sum + part.exercises
+  })
+
+  return(
+    <p>Number of excercises {sum}</p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return(
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App;
