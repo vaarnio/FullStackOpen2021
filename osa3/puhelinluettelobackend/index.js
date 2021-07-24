32dfdf3068c4049c6992d2f7b8a970e6df848475
Valtteri A <vjaarn@utu.fi>
2021-07-24 09:09:40 +0300

    3.4 valmis

--- osa3/puhelinluettelobackend/index.js
+++ osa3/puhelinluettelobackend/index.js
@@ -42,6 +42,13 @@ app.get('/api/persons/:id', (req, res) => {
         response.status(404).end()
     }
 })
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    res.status(204).end()
+})
   
 const PORT = 3001
 app.listen(PORT, () => {
