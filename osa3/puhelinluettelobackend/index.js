7a5f6a2b4c7259f453a23311a44ff5aeabc8dd81
Valtteri A <vjaarn@utu.fi>
2021-07-25 10:01:11 +0300

    puhelinluettelon production build lisätty backendin hakemistoon

--- osa3/puhelinluettelobackend/index.js
+++ osa3/puhelinluettelobackend/index.js
@@ -6,6 +6,7 @@ morgan.token('res-body', (req, res) => res.resBody)
 
 const app = express()
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res-body'))
 
