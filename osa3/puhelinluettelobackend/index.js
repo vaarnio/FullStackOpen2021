e94a92b5598d7d8922bd366422cf55fde97240a5
Valtteri Aarnio <github@vaarn.io>
2021-09-28 18:47:13 +0300

    3.22 done, uibuilt and many style fixes

--- osa3/puhelinluettelobackend/index.js
+++ osa3/puhelinluettelobackend/index.js
@@ -13,96 +13,96 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res-body'))
 
 app.get('/info', (req, res, next) => {
-    Person.countDocuments()
-        .then(count => {
-            res.send(`Phonebook has info for ${count} people<br/><br/>${new Date()}`)
-        })
-        .catch(error => next(error))
-})
-
-app.get('/api/persons', (req, res, next) => {
-    Person.find({}).then(persons => {
-        res.json(persons)
+  Person.countDocuments()
+    .then(count => {
+      res.send(`Phonebook has info for ${count} people<br/><br/>${new Date()}`)
     })
     .catch(error => next(error))
 })
 
+app.get('/api/persons', (req, res, next) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+    .catch(error => next(error))
+})
+
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id)
-        .then(result => {
-            if(result){
-                res.json(result)
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findById(req.params.id)
+    .then(result => {
+      if(result){
+        res.json(result)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-        .then(result => {
-            if(result){
-                res.status(204).end()
-            } else {
-                res.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      if(result){
+        res.status(204).end()
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    //add person to phonebook and generate a random id for them
-    const body = req.body
+  //add person to phonebook and generate a random id for them
+  const body = req.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person
+    .save()
+    .then(savedPerson => savedPerson.toJSON())
+    .then(savedAndFormattedPerson => {
+      res.json(savedAndFormattedPerson)
     })
-
-    person
-        .save()
-        .then(savedPerson => savedPerson.toJSON())
-        .then(savedAndFormattedPerson => {
-            res.json(savedAndFormattedPerson)
-        })
-        .catch(error => {
-            console.log(error)
-            next(error)
-        })
+    .catch(error => {
+      next(error)
+    })
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    body = req.body
+  //update entry in phonebook
+  const body = req.body
 
-    Person.findByIdAndUpdate(req.params.id, body, {new: true})
-        .then(updatedPerson => {
-            res.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(req.params.id, body, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
-    res.status(404).send({error: 'unknown endpoint'})
+  res.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.log(error.message)
+  console.log(error.message)
 
-    if(error.name === 'CastError') {
-        return res.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({error: error.message})
-    }
+  if(error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
-  
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- osa3/puhelinluettelobackend/models/person.js
+++ osa3/puhelinluettelobackend/models/person.js
@@ -5,31 +5,31 @@ const uniqueValidator = require('mongoose-unique-validator')
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })  .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        unique: true
-    },
-    number:  {
-        type: String,
-        minlength: 8,
-    },
+  name: {
+    type: String,
+    minlength: 3,
+    unique: true
+  },
+  number:  {
+    type: String,
+    minlength: 8,
+  },
 })
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
