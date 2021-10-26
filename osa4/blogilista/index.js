6c5a6350efda6f57a6cec1022d6be6d56dede7de
Valtteri Aarnio <github@vaarn.io>
2021-10-26 11:04:11 +0300

    routejen määrittely eriytetty omaan tiedostoonsa

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -4,28 +4,12 @@ const config = require('./utils/config')
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const blogsRouter = require('./controllers/blogs')
 const Blog = require('./models/blog')
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = config.PORT
 app.listen(PORT, () => {
