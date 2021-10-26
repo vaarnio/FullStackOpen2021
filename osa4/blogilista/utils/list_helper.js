fc1f255ca59b75ee067260c960a8a7da3336ed85
Valtteri Aarnio <github@vaarn.io>
2021-10-26 13:51:47 +0300

    4.4 done

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
-    // ...
+    return 1
+}
+
+const totalLikes = (blogs) => {
+    const likes = blogs.map(x => x.likes)
+    const reducer = (sum, next) => {return sum + next}
+    return likes.reduce(reducer)
 }
 
 module.exports = {
-    dummy
+    dummy, totalLikes
 }
